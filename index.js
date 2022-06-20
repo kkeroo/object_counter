5c696b038c2e05f69ad9dc2dcf6cda0d7de65b43
kkeroo <jasko10.kerec@gmail.com>
2022-06-20 17:53:37 +0200

    Začetna verzija.

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const PORT = process.env.PORT | 4000;
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
