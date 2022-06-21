bbf63f19ed1f8b00b14e07293b7480276de7f7bf
kkeroo <jasko10.kerec@gmail.com>
2022-06-21 17:20:46 +0200

    Dodana različna orodja ter shranjevanje stanja.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import logo from './logo.svg';
-import slika from './slika.jpg';
+import slika from './slika4.jpeg';
 import './App.css';
 import * as markerjs2 from 'markerjs2';
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 class App extends Component {
   constructor(props){
@@ -18,10 +22,11 @@ class App extends Component {
 
       // Settings
       this.markerArea.settings.defaultColor = 'green';
+      this.markerArea.settings.defaultFillColor = 'transparent';
       this.markerArea.uiStyleSettings.toolbarHeight = 0;
       this.markerArea.uiStyleSettings.hideToolbar = true;
       this.markerArea.uiStyleSettings.hideBox = true;
-      this.markerArea.zoomSteps = [1, 1.5, 2, 4];
+      this.markerArea.zoomSteps = [1, 1.5, 2, 2.5, 3];
       this.markerArea.uiStyleSettings.zoomButtonVisible = true;
       this.markerArea.settings.defaultStrokeWidth = 1;
       this.markerArea.settings.newFreehandMarkerOnPointerUp = true;
@@ -29,29 +34,55 @@ class App extends Component {
 
       // attach an event handler to assign annotated image back to our image element
       this.markerArea.addEventListener('render', event => {
-        if (this.imgRef.current) {
-          this.imgRef.current.src = event.dataUrl;
-        }
+        //if (this.imgRef.current) {
+        //  this.imgRef.current.src = event.dataUrl;
+        //}
+        console.log(event);
+        let myState = event.state;
+        this.showMarkerArea();
+        this.markerArea.restoreState(myState);
       });
+
       // launch marker.js
       this.markerArea.show();
     }
   }
 
+  zoom(e) {
+    let deltaY = e.deltaY;
+    console.log(deltaY);
+    if (deltaY > 1){
+      this.markerArea.stepZoom();
+    }
+  }
+
+  finishEditing() {
+    this.markerArea.startRenderAndClose();
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>marker.js 2 Demo.</h1>
-        <button onClick = {() => this.showMarkerArea()}>Začni</button>
-        <button onClick={() => { this.markerArea.createNewMarker(markerjs2.FrameMarker); }}>Rectangle</button>
-        <button onClick={() => { this.markerArea.createNewMarker(markerjs2.EllipseFrameMarker); }}>Ellipse</button>
-        <button onClick={() => { this.markerArea.stepZoom(); }}>Zoom in</button>
-        <img ref={this.imgRef} 
-          src={slika} 
-          alt="sample" 
-          style={{ maxWidth: '50%' }}
-          //onClick = {() => this.showMarkerArea()}
+        <Navbar bg="dark" expand="lg" variant="dark">
+          <Container>
+            <Navbar.Brand>Object Counter</Navbar.Brand>
+          </Container>
+        </Navbar>
+        <Container className="mt-3">
+          <Button className="me-2 btn-success" onClick = {() => this.showMarkerArea()}>Start</Button>
+          <Button className="me-2" onClick={() => { this.markerArea.createNewMarker(markerjs2.FrameMarker); }}>Rectangle</Button>
+          <Button className="me-2" onClick={() => { this.markerArea.createNewMarker(markerjs2.EllipseMarker); }}>Ellipse</Button>
+          <Button className="me-2" onClick={() => { this.markerArea.stepZoom(); }}>Zoom in</Button>
+          <Button className="btn-danger" onClick={() => { this.finishEditing(); }}>Finish</Button>
+        </Container>
+        <div className="img-container">
+          <img ref={this.imgRef}
+            className="slika"
+            src={slika}
+            alt="sample"
+            onWheel={(e) => {this.zoom(e)}}
           />
+        </div>
       </div>
     );
   }
