7092f8181b8c1ee1679686b85e0ae18df62d07cc
kkeroo <jasko10.kerec@gmail.com>
2022-06-22 11:40:05 +0200

    Izboljšan izgled.

--- client/src/components/LoadDataset.js
+++ client/src/components/LoadDataset.js
@@ -8,6 +8,7 @@ import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 import Stack from 'react-bootstrap/Stack';
+import Alert from 'react-bootstrap/Alert';
 
 const LoadDataset = () => {
     const [images, setImages] = useState(new Array());
@@ -33,18 +34,32 @@ const LoadDataset = () => {
         <div className="LoadDataset">
             <Navbar></Navbar>
             <Container className="mt-3">
-                <Form className='file-input'>
-                    <Form.Group controlId="formFile" className="mb-2">
-                        <Form.Control type="file" multiple accept=".jpg" onChange={(e) => {imageSelected(e);}}/>
-                    </Form.Group>
-                </Form>
+                <Row className="mb-4 mt-4">
+                    <Col lg="1"></Col>
+                    <Col lg="10">
+                        <Row>
+                            <Col>
+                                <Form className='file-input me-3 mt-1'>
+                                    <Form.Group controlId="formFile" className="mb-2">
+                                        <Form.Control type="file" multiple accept=".jpg" onChange={(e) => {imageSelected(e);}}/>
+                                    </Form.Group>
+                                </Form>
+                            </Col>
+                            <Col>
+                                <p className="" variant="light">Create an image dataset. Choose files to add them to the dataset. You can choose
+                                multiple files, multiple times.</p>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col lg="1"></Col>
+                </Row>
                 <Row>
                     <Col sm="2" lg="2"></Col>
                     <Col sm="6" lg="6">
                         <img className="preview-image" src={previewImage != "" ? URL.createObjectURL(previewImage) : ""}/>
                     </Col>
                     <Col sm="4" lg="4" className="images-list-container">
-                        <Stack direction='horizontal' gap="3" className="s">
+                        <Stack direction='horizontal' gap="3">
                             <h5>Uploaded images</h5>
                             <Button size="sm" className="btn-danger mb-2 ms-auto" onClick={() => {deleteDataset();}}>Delete dataset</Button>
                         </Stack>
