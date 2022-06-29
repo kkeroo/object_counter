66f836a52d6c5ce1783a19643ba50953b3ebe48e
kkeroo <jasko10.kerec@gmail.com>
2022-06-29 12:05:14 +0200

    Dodano serviranje reacta builda v nodeJs.

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const path = require('path');
+var compression = require('compression');
 const express = require("express");
+
 const PORT = process.env.PORT | 4000;
 const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(compression());
+
+// using https on production server
+if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https")
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    else next();
+  });
+}
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
